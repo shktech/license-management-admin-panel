a186ff9282a8bf4672dadbc0a59fbbd6b167e81d
le-roi-dev <chry.dev1220@outlook.com>
2024-08-19 20:37:07 +0900

    Organization Page

--- src/app/orgs/page.tsx
+++ src/app/orgs/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Accordion, AccordionSummary, AccordionDetails } from "@mui/material";
+import { Accordion, AccordionSummary, AccordionDetails, Box, IconButton } from "@mui/material";
 import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp";
 import CorporateFareIcon from "@mui/icons-material/CorporateFare";
 import { HttpError, useGetIdentity, useList } from "@refinedev/core";
@@ -8,13 +8,19 @@ import { useForm } from "@refinedev/react-hook-form";
 import { Organization, User } from "@/types/types";
 import GenericForm from "@components/Forms/GenericForm";
 import { OrganizationEditFormFields } from "@components/Forms/Organizations/OrganizationFormFields";
-import { useEffect, useState } from "react";
-import { tableAddButton } from "@data/MuiStyles";
+import { useEffect, useMemo, useState } from "react";
+import { tableAddButton, tagStyle } from "@data/MuiStyles";
 import { Button } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import { modalOkBtnStyle } from "@data/MuiStyles";
 import OrganizationDetailDrawer from "@components/Organizations/OrganizationDrawer";
 import OrganizationComponent from "@components/Organizations/OrganizationComponent";
+import { MRT_ColumnDef } from "material-react-table";
+import { ProductActiveColor } from "@data/ColorData";
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
+import DeleteIcon from '@mui/icons-material/Delete';
+import GenericTable from "@components/Table/GenericTable";
+import Loader from "@components/common/Loader";
 
 const Page = () => {
   const { data: identity, isLoading: isIdentityLoading } =
@@ -29,58 +35,129 @@ const Page = () => {
   });
 
   useEffect(() => {
-    if (identity && orgs) {
-      const currentOrg = orgs.data?.find(
-        (org) => org.organization_code === identity.organization
-      );
-      const otherOrgs = orgs.data?.filter(
-        (org) => org.organization_code !== identity.organization
-      );
-      setCurrentOrg(currentOrg as Organization);
-      setOtherOrgs(otherOrgs);
-    }
-  }, [identity, orgs]);
-
-  useEffect(() => {
     setLoading(isIdentityLoading || isLoading);
   }, [isIdentityLoading, isLoading]);
 
-  const [currentOrg, setCurrentOrg] = useState<Organization | null>(null);
-  const [otherOrgs, setOtherOrgs] = useState<Organization[]>([]);
+  const [orgData, setOrgData] = useState<any[]>();
   const [loading, setLoading] = useState(true);
   const [openDrawer, setOpenDrawer] = useState(false);
+  const [clickedOrg, setClickedOrg] = useState<Organization | null>(null);
 
+  useEffect(() => {
+    if (identity && orgs) {
+      setOrgData(orgs?.data.map((org: Organization) => ({
+        ...org,
+        isCurrent: org.organization_code === identity.organization
+      })))
+    }
+  }, [identity, orgs]);
+
+  const handleEditClick = (row: Organization) => {
+    setClickedOrg(row);
+    setOpenDrawer(true);
+  };
+  const handleCreate = () => setOpenDrawer(true);
   const handleClose = () => {
     refetch();
+    setClickedOrg(null);
     setOpenDrawer(false);
-  };
-
-  return (
-    <div>
-      <div className="flex flex-row gap-4 items-center px-12 py-8">
-        <div className="text-2xl font-semibold">Org Management</div>
-      </div>
-      <div className="flex justify-center">
-        <div className="w-2/3 flex flex-col gap-2">
-          <div className="flex justify-between items-start">
-            <div className="flex flex-col">
-              <div className="text-lg font-semibold">Organizations</div>
-              <div className="text-sm">
-                The organizations that are associated with your account
+  }
+  const handleDeleteBtn = (org: Organization) => {
+    // handleOpenDeleteModal();
+    // setClickedOrg(org);
+  }
+  const columns = useMemo<MRT_ColumnDef<any>[]>(
+    () => [
+      {
+        accessorKey: "organization_code",
+        header: "Organization Code",
+        size: 200,
+      },
+      {
+        accessorKey: "organization_name",
+        header: "Name",
+        size: 150,
+      },
+      {
+        accessorKey: "address",
+        header: "Address",
+        size: 150,
+      },
+      {
+        accessorKey: "country",
+        header: "Country",
+        size: 150,
+      },
+      {
+        accessorKey: "active",
+        header: "Active",
+        size: 100,
+        Cell: ({ renderedCellValue }) => (
+          <Box component="span" sx={{ backgroundColor: ProductActiveColor(renderedCellValue as boolean), ...tagStyle }} >
+            {renderedCellValue ? "Active" : "Closed"}
+          </Box>
+        ),
+      },
+      {
+        accessorKey: "actions",
+        header: "Action",
+        size: 100,
+        enableSorting: false,
+        pin: 'right',
+        Cell: ({ row }) => {
+          return (
+            <div className="w-full h-full">
+              <div className="flex gap-1">
+                <IconButton
+                  onClick={() => handleEditClick(row.original)}
+                  size="small"
+                  disabled={!row.original?.isCurrent}
+                >
+                  <EditOutlinedIcon fontSize="inherit" />
+                </IconButton>
+                <IconButton
+                  onClick={() => handleDeleteBtn(row.original)}
+                  size="small"
+                  disabled={!row.original?.isCurrent}
+                >
+                  <DeleteIcon fontSize="inherit" />
+                </IconButton>
               </div>
             </div>
-            <Button onClick={() => setOpenDrawer(true)} sx={tableAddButton}>
-              <AddIcon /> {"Create an Organization"}
-            </Button>
-          </div>
-          {([currentOrg, ...otherOrgs] as Organization[]).map(
-            (org: Organization, i: number) => (
-              <OrganizationComponent organization={org} enableSave={i === 0} />
-            )
-          )}
-        </div>
-      </div>
-      <OrganizationDetailDrawer open={openDrawer} onClose={handleClose} />
+          )
+        },
+      },
+    ],
+    []
+  );
+
+  return (
+    <div className="pt-6 pb-2.5 xl:pb-1 overflow-x-auto">
+      {
+        loading ?
+          <Loader /> :
+          <GenericTable
+            title={
+              <div className="flex gap-4 items-end">
+                <div className="text-lg font-semibold">Organizations</div>
+                <div className="text-sm font-normal">
+                  ( The organizations that are associated with your account )
+                </div>
+              </div>
+            }
+            data={orgData}
+            columns={columns}
+            canCreate={true}
+            noSearchNeed
+            totalCount={orgs?.total}
+            handleCreate={handleCreate}
+          />
+      }
+      <OrganizationDetailDrawer
+        open={openDrawer}
+        onClose={handleClose}
+        org={clickedOrg}
+      />
     </div>
   );
 };
