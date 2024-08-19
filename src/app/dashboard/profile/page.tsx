1c1bf2332dd73c2e2e597e4956c286027705b82b
le-roi-dev <chry.dev1220@outlook.com>
2024-08-20 00:13:31 +0900

    profile page ui

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -7,9 +7,9 @@ import { RoleColors } from "@data/ColorData";
 import { useCreate, useCustom, useCustomMutation, useDelete, useGetIdentity, useList } from "@refinedev/core";
 import { useForm } from "@refinedev/react-hook-form";
 import { getReadableDate, truncateString } from "@utils/utilFunctions";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ContentCopyOutlinedIcon from '@mui/icons-material/ContentCopyOutlined';
-import { Button, IconButton } from "@mui/material";
+import { Box, Button, IconButton, Tab, Tabs, Typography } from "@mui/material";
 import { modalOkBtnStyle } from "@data/MuiStyles";
 
 const Page = () => {
@@ -18,6 +18,11 @@ const Page = () => {
     resource: "orgs/key/api-key",
     hasPagination: false
   });
+  const [page, setPage] = useState(0);
+
+  const handleChangePage = (event: React.SyntheticEvent, newValue: number) => {
+    setPage(newValue);
+  };
 
   const apiKey = apiData?.data as APIKey;
 
@@ -104,8 +109,80 @@ const Page = () => {
     reset(identity);
   }, [identity]);
 
+  const panels = [
+    {
+      label: "General Information",
+      value: (
+        <>
+          <div className="text-xl font-semibold pb-4">User Information</div>
+          <ProfileForm
+            {...{ control, errors, trigger }}
+          />
+        </>
+      )
+    },
+    {
+      label: "User Role",
+      value: (
+        <>
+          <div className="text-xl font-semibold pb-8">
+            User Role
+            <span
+              className={`${RoleColors[identity?.roles?.[0].name as string] || RoleColors["default"]} text-white mx-4 px-4 py-1 text-xs font-bold rounded-full`}
+            >
+              {identity?.roles?.[0].name}
+            </span>
+          </div>
+          <PermissionsTable
+            permissions={identity?.roles?.[0].permissions as Permission[]}
+            readonly
+          />
+        </>
+      )
+    },
+    {
+      label: 'Personal API Key',
+      value: (
+        <>
+          <div className="text-xl font-semibold pb-8 flex justify-between">
+            Personal API Keys
+            {
+              apiData?.data ?
+                <Button
+                  variant="contained"
+                  sx={modalOkBtnStyle}
+                  onClick={handleDeleteKeyBtn}
+                >
+                  Delete
+                </Button> :
+                <Button
+                  variant="contained"
+                  sx={modalOkBtnStyle}
+                  onClick={handleGenerateBtn}
+                >
+                  Generate Key
+                </Button>
+            }
+          </div>
+          <div className="pb-6">
+            {
+              apiData?.data ?
+                apiDataKeys.map(adk => (
+                  <div className="grid grid-cols-3 border-b border-[#d5dce3] py-1.5 px-8 items-center justify-center">
+                    <div className="font-medium">{adk.label}</div>
+                    <div className="col-span-2">{adk.value}</div>
+                  </div>
+                )) :
+                <div className="flex items-center justify-center text-[#848fa7] text-lg font-medium">There is no API Key</div>
+            }
+          </div>
+        </>
+      )
+    }
+  ]
+
   return (
-    <div className="">
+    <div className="flex flex-col">
       <div className="flex gap-4 items-center px-12 py-8">
         <div className="text-white bg-[#1f325c] font-bold text-4xl w-20 h-20 flex items-center justify-center rounded-full">{`${identity?.first_name?.[0]} ${identity?.last_name?.[0]}`}</div>
         <div className="">
@@ -119,68 +196,22 @@ const Page = () => {
         </div>
 
       </div>
-      <div className="grid grid-cols-2 gap-8 px-8">
-        <div className="">
-          <div className="p-6 border-[#1f325c] border-t-4 shadow-lg rounded-lg bg-white">
-            <div className="text-xl font-semibold pb-4">User Information</div>
-            <ProfileForm
-              {...{ control, errors, trigger }}
-            />
+
+      <div className="flex flex-1 gap-8 px-8">
+        <div className="min-w-72">
+          <div className="bg-white rounded-lg px-3 gap-3 flex flex-col py-3">
+            {
+              panels.map((panel, i) => (
+                <div onClick={() => setPage(i)} className={`px-2 py-2 border-b-2 hover:border-[#1f325c] cursor-pointer font-medium duration-500 ${page != i ? 'border-transparent' : 'border-[#1f325c]'}`}>{panel.label}</div>
+              ))
+            }
           </div>
         </div>
-        <div className="">
-          <div className="p-6 border-[#1f325c] border-t-4 shadow-lg rounded-lg bg-white w-full">
-            <div className="text-xl font-semibold pb-8">
-              User Role
-              <span
-                className={`${RoleColors[identity?.roles?.[0].name as string] || RoleColors["default"]} text-white mx-4 px-4 py-1 text-xs font-bold rounded-full`}
-              >
-                {identity?.roles?.[0].name}
-              </span>
-            </div>
-            <PermissionsTable
-              permissions={identity?.roles?.[0].permissions as Permission[]}
-              readonly
-            />
-          </div>
-
-          <div className="py-6 border-[#1f325c] border-t-4 shadow-lg rounded-lg bg-white mt-8 w-full">
-            <div className="text-xl px-6 font-semibold pb-8 flex justify-between">
-              Personal API Keys
-              {
-                apiData?.data ?
-                  <Button
-                    variant="contained"
-                    sx={modalOkBtnStyle}
-                    onClick={handleDeleteKeyBtn}
-                  >
-                    Delete
-                  </Button> :
-                  <Button
-                    variant="contained"
-                    sx={modalOkBtnStyle}
-                    onClick={handleGenerateBtn}
-                  >
-                    Generate Key
-                  </Button>
-              }
-            </div>
-            <div>
-              {
-                apiData?.data ?
-                  apiDataKeys.map(adk => (
-                    <div className="grid grid-cols-3 border-b border-[#d5dce3] py-1.5 px-8 items-center justify-center">
-                      <div className="font-medium">{adk.label}</div>
-                      <div className="col-span-2">{adk.value}</div>
-                    </div>
-                  )) :
-                  <div className="flex items-center justify-center text-[#848fa7] text-lg font-medium">There is no API Key</div>
-              }
-
-            </div>
-          </div>
+        <div className="px-8 py-4 flex-1 bg-white rounded-xl">
+          {panels[page].value}
         </div>
       </div>
+
     </div>
   );
 };
