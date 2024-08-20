8b74233c1879438e6d8dadfbf0697f989d5321fc
le-roi-dev <chry.dev1220@outlook.com>
2024-08-20 17:07:43 +0300

    Profile page/Personal API Key Part Completed

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -1,16 +1,12 @@
 "use client";
-import { APIKey, Permission, User } from "@/types/types";
-import GeneralInformation from "@components/common/View/GeneralInformation";
+import { Permission, User } from "@/types/types";
 import ProfileForm from "@components/Forms/Profile/ProfileForm";
 import PermissionsTable from "@components/Role/PermissionsTable";
 import { RoleColors } from "@data/ColorData";
-import { useCreate, useCustom, useCustomMutation, useDelete, useGetIdentity, useList } from "@refinedev/core";
+import { useGetIdentity, useList } from "@refinedev/core";
 import { useForm } from "@refinedev/react-hook-form";
-import { getReadableDate, truncateString } from "@utils/utilFunctions";
 import { useEffect, useState } from "react";
-import ContentCopyOutlinedIcon from '@mui/icons-material/ContentCopyOutlined';
-import { Box, Button, IconButton, Tab, Tabs, Typography } from "@mui/material";
-import { modalOkBtnStyle } from "@data/MuiStyles";
+import APIKeyPanel from "@components/Profile/APIKeyPanel";
 
 const Page = () => {
   const { data: identity } = useGetIdentity<User>();
@@ -20,82 +16,6 @@ const Page = () => {
   });
   const [page, setPage] = useState(0);
 
-  const handleChangePage = (event: React.SyntheticEvent, newValue: number) => {
-    setPage(newValue);
-  };
-
-  const apiKey = apiData?.data as APIKey;
-
-  const { mutate: createAPIKey } = useCreate();
-  const { mutate: deleteAPIKey } = useCustomMutation();
-
-  const handleGenerateBtn = () => {
-    const handleError = (error: any) => {
-
-    };
-    createAPIKey(
-      {
-        resource: "orgs/key/api-key",
-        values: {}
-      },
-      {
-        onError: () => refetch(),
-        onSuccess: () => refetch()
-      }
-    )
-  }
-
-  const handleDeleteKeyBtn = () => {
-    const handleError = (error: any) => {
-
-    };
-    deleteAPIKey(
-      {
-        url: 'orgs/key/api-key',
-        method: 'delete',
-        values: {
-          id: apiKey.id
-        }
-      },
-      {
-        onError: handleError,
-        onSuccess: () => refetch(),
-      }
-    )
-  }
-
-  const handleCopyAPIKey = () => {
-    navigator.clipboard.writeText(apiKey?.id as string);
-  }
-  const apiDataKeys = [
-    {
-      key: "id",
-      label: "API Key ID",
-      value: (
-        <div className="flex items-center justify-center gap-4">
-          <div className="truncated">{apiKey?.id}</div>
-          <IconButton aria-label="copy" onClick={handleCopyAPIKey}>
-            <ContentCopyOutlinedIcon fontSize="small" />
-          </IconButton>
-        </div>
-      )
-    },
-    {
-      key: "name",
-      label: "Name",
-      value: apiKey?.name
-    },
-    {
-      key: "created",
-      label: "Created at",
-      value: getReadableDate(apiKey?.created)
-    },
-    {
-      key: "revoked",
-      label: "Revoked",
-      value: <div className={`rounded-full h-4 w-4 ${apiKey?.revoked ? 'bg-[#11ba82]' : 'bg-[#929ea8]'}`}></div>
-    }
-  ]
 
   const {
     control,
@@ -143,40 +63,7 @@ const Page = () => {
     {
       label: 'Personal API Key',
       value: (
-        <>
-          <div className="text-xl font-semibold pb-8 flex justify-between">
-            Personal API Keys
-            {
-              apiData?.data ?
-                <Button
-                  variant="contained"
-                  sx={modalOkBtnStyle}
-                  onClick={handleDeleteKeyBtn}
-                >
-                  Delete
-                </Button> :
-                <Button
-                  variant="contained"
-                  sx={modalOkBtnStyle}
-                  onClick={handleGenerateBtn}
-                >
-                  Generate Key
-                </Button>
-            }
-          </div>
-          <div className="pb-6">
-            {
-              apiData?.data ?
-                apiDataKeys.map(adk => (
-                  <div className="grid grid-cols-3 border-b border-[#d5dce3] py-1.5 px-8 items-center justify-center">
-                    <div className="font-medium">{adk.label}</div>
-                    <div className="col-span-2">{adk.value}</div>
-                  </div>
-                )) :
-                <div className="flex items-center justify-center text-[#848fa7] text-lg font-medium">There is no API Key</div>
-            }
-          </div>
-        </>
+        <APIKeyPanel />
       )
     }
   ]
---
+++ src/components/Profile/APIKeyPanel.tsx
@@ -0,0 +1,132 @@
+import { alpha, Box, Button, Modal, SnackbarCloseReason } from "@mui/material";
+import { useCreate, useCustomMutation, useList } from "@refinedev/core";
+import React, { useState } from "react";
+import { getReadableDate } from "@utils/utilFunctions";
+import { modalOkBtnStyle, modalStyle } from "@data/MuiStyles";
+import CheckBoxRoundedIcon from '@mui/icons-material/CheckBoxRounded';
+import { APIKey } from "../../types/types";
+import RedditTextField from "@components/common/RedditTextField";
+import SnackbarComponent from "@components/common/SnackbarComponent";
+
+interface APIKeyPanelProps { }
+
+const APIKeyPanel: React.FC<APIKeyPanelProps> = () => {
+    const { data: apiData, refetch, isLoading } = useList({
+        resource: "orgs/key/api-key",
+        hasPagination: false,
+    });
+
+    const [generatedAPIKey, setGeneratedAPIKey] = useState('');
+    const [openDeleteModal, setOpenDeleteModal] = useState(false);
+    const [openSnackbar, setOpenSnackbar] = useState(false);
+
+    const handleOpenDeleteModal = () => setOpenDeleteModal(true);
+    const handleCloseDeleteModal = () => setOpenDeleteModal(false);
+    const handleSuccessGenerated = (res: any) => {
+        handleOpenDeleteModal();
+        setGeneratedAPIKey(res.data.key);
+    };
+
+    const handleSnackbarOpen = () => setOpenSnackbar(true);
+    const handleSnackbarClose = (
+        event: React.SyntheticEvent | Event,
+        reason?: SnackbarCloseReason,
+    ) => {
+        if (reason === 'clickaway') return;
+        setOpenSnackbar(false);
+    };
+
+    const apiKey = apiData?.data as APIKey;
+
+    const { mutate: createAPIKey } = useCreate();
+    const { mutate: deleteAPIKey } = useCustomMutation();
+
+    const handleGenerateBtn = () => {
+        createAPIKey(
+            {
+                resource: "orgs/key/api-key",
+                values: {},
+            },
+            {
+                onError: () => refetch(),
+                onSuccess: (res) => handleSuccessGenerated(res),
+            }
+        );
+    };
+
+    const handleDeleteKeyBtn = () => {
+        deleteAPIKey(
+            {
+                url: 'orgs/key/api-key',
+                method: 'delete',
+                values: { id: apiKey.id },
+            },
+            {
+                onError: (error) => console.error(error),
+                onSuccess: () => refetch(),
+            }
+        );
+    };
+
+    const apiDataKeys = [
+        { key: "name", label: "Name", value: apiKey?.name },
+        { key: "created", label: "Created at", value: getReadableDate(apiKey?.created) },
+        {
+            key: "revoked",
+            label: "Revoked",
+            value: <div className={`rounded-full h-4 w-4 ${apiKey?.revoked ? 'bg-[#11ba82]' : 'bg-[#929ea8]'}`}></div>,
+        },
+    ];
+
+    return (
+        <>
+            <div className="text-xl font-semibold pb-8 flex justify-between">
+                Personal API Keys
+                {apiData?.data ?
+                    <Button onClick={handleDeleteKeyBtn} variant="contained" sx={modalOkBtnStyle}>Delete</Button> :
+                    <Button onClick={handleGenerateBtn} variant="contained" sx={modalOkBtnStyle}>Generate Key</Button>
+                }
+            </div>
+            <div className="pb-6">
+                {apiData?.data ? (
+                    apiDataKeys.map((adk) => (
+                        <div key={adk.key} className="grid grid-cols-3 border-b border-[#d5dce3] py-1.5 px-8 items-center justify-center">
+                            <div className="font-medium">{adk.label}</div>
+                            <div className="col-span-2">{adk.value}</div>
+                        </div>
+                    ))
+                ) : (
+                    <div className="flex items-center justify-center text-[#848fa7] text-lg font-medium">
+                        There is no API Key
+                    </div>
+                )}
+            </div>
+            <Modal
+                open={openDeleteModal}
+                onClose={handleCloseDeleteModal}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={{ ...modalStyle, width: 500 }}>
+                    <div className="text-base text-[#11ba82] font-medium flex items-center gap-1">
+                        <CheckBoxRoundedIcon />
+                        Successfully Generated
+                    </div>
+                    <RedditTextField
+                        label="API Key"
+                        value={generatedAPIKey}
+                        variant="filled"
+                        style={{ marginTop: 11 }}
+                        onCopy={handleSnackbarOpen}
+                    />
+                </Box>
+            </Modal>
+            <SnackbarComponent
+                open={openSnackbar}
+                onClose={handleSnackbarClose}
+            />
+        </>
+    );
+};
+
+export default APIKeyPanel;
