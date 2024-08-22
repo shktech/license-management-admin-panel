1a61989c8dfc13b814101fe7e8982f8b5bcf5426
le-roi-dev <chry.dev1220@outlook.com>
2024-08-22 21:16:40 +0300

    add New Customer function in Transaction Form

--- src/components/Forms/Transactions/TransactionForm.tsx
+++ src/components/Forms/Transactions/TransactionForm.tsx
@@ -17,6 +17,7 @@ import { LicensingDetailFormFields } from "./LicensingDetailFormFields";
 import { useEffect, useState } from "react";
 import { Autocomplete, Box, TextField } from "@mui/material";
 import { useOne, useTable } from "@refinedev/core";
+import { TURBO_TRACE_DEFAULT_MEMORY_LIMIT } from "next/dist/shared/lib/constants";
 
 export type TransactionFormProps = GenericFormProps & {
   transaction?: Transaction;
@@ -24,45 +25,77 @@ export type TransactionFormProps = GenericFormProps & {
   reset?: any
 };
 
+const ADDLABEL = "New Customer";
 
 const TransactionForm = ({ reset, ...props }: TransactionFormProps) => {
 
-  const [value, setValue] = useState<Customer | null>();
-  const [inputValue, setInputValue] = useState('');
-  const [customers, setCustomers] = useState<Customer[]>([]);
+  const [value, setValue] = useState<any>({
+    isAdd: true,
+    account: ADDLABEL
+  });
+  const [inputValue, setInputValue] = useState(ADDLABEL);
+  const [customers, setCustomers] = useState<any[]>([]);
   const {
     tableQueryResult: { data, isLoading },
     setFilters,
   } = useTable<Customer>({
-    resource: "customers/bill-customers"
+    resource: "customers/bill-customers",
+    syncWithLocation: false
   });
 
-  const handleValueChange = (event: any, newValue: Customer | null) => {
+  const handleValueChange = (event: any, newValue: any) => {
     setValue(newValue);
-    reset({
-      bill_customer_account: newValue?.account,
-      bill_customer_name: newValue?.name,
-      bill_address1: newValue?.contact.address?.address1,
-      bill_address2: newValue?.contact.address?.address2,
-      bill_city: newValue?.contact.address?.city,
-      bill_state: newValue?.contact.address?.state,
-      bill_postal_code: newValue?.contact.address?.postal_code,
-      bill_country: newValue?.contact.address?.country,
-      bill_contact_first_name: newValue?.contact.first_name,
-      bill_contact_last_name: newValue?.contact.last_name,
-      bill_contact_phone: newValue?.contact.phone,
-      bill_contact_email: newValue?.contact.email,
-    });
   }
+
+
+  useEffect(() => {
+    if (value?.isAdd) {
+      reset({
+        bill_customer_account: '',
+        bill_customer_name: '',
+        bill_address1: '',
+        bill_address2: '',
+        bill_city: '',
+        bill_state: '',
+        bill_postal_code: '',
+        bill_country: '',
+        bill_contact_first_name: '',
+        bill_contact_last_name: '',
+        bill_contact_phone: '',
+        bill_contact_email: '',
+      })
+    } else {
+      reset({
+        bill_customer_account: value?.account,
+        bill_customer_name: value?.name,
+        bill_address1: value?.contact.address?.address1,
+        bill_address2: value?.contact.address?.address2,
+        bill_city: value?.contact.address?.city,
+        bill_state: value?.contact.address?.state,
+        bill_postal_code: value?.contact.address?.postal_code,
+        bill_country: value?.contact.address?.country,
+        bill_contact_first_name: value?.contact.first_name,
+        bill_contact_last_name: value?.contact.last_name,
+        bill_contact_phone: value?.contact.phone,
+        bill_contact_email: value?.contact.email,
+      });
+    }
+  }, [value])
 
   const handleSearch = (value: string) => setFilters([{ field: 'searchKey', operator: 'contains', value: value }])
   const handleInputChange = (event: any, newInputValue: any) => {
     setInputValue(newInputValue);
-    handleSearch(newInputValue)
+    if (newInputValue == ADDLABEL) {
+      handleSearch('')
+    } else {
+      handleSearch(newInputValue)
+    }
   }
 
   useEffect(() => {
-    setCustomers(data?.data as Customer[])
+    if (data?.data) {
+      setCustomers(data?.data as any[])
+    }
   }, [data])
 
   return (
@@ -74,9 +107,15 @@ const TransactionForm = ({ reset, ...props }: TransactionFormProps) => {
         sx={{ width: 300 }}
         inputValue={inputValue}
         onInputChange={handleInputChange}
-        options={customers}
-        autoHighlight
+        options={[
+          {
+            isAdd: true,
+            account: ADDLABEL
+          },
+          ...customers
+        ]}
         loading={isLoading}
+        filterOptions={(options) => options}
         getOptionLabel={(option) => option.account as string}
         renderOption={(props, option) => {
           const { key, ...optionProps } = props;
@@ -86,31 +125,30 @@ const TransactionForm = ({ reset, ...props }: TransactionFormProps) => {
               component="li"
               {...optionProps}
             >
-              <div className="text-sm flex flex-col gap-2 py-2">
-                <div className="flex gap-4">
-                  <div className="border-b">{option.account}</div>
-                  <div className="border-b">{option.contact.first_name + " " + option.contact.last_name}</div>
-                </div>
-                <div className="flex gap-2">
-                  <div className="border-b">{option.contact.address?.address1}</div>
-                  <div className="border-b">{option.contact.address?.address2}</div>
-                  <div className="border-b">{option.contact.address?.state}</div>
-                  <div className="border-b">{option.contact.address?.country}</div>
-                </div>
-              </div>
+              {
+                option.isAdd == true ?
+                  <div className="border-b">{option.account}</div> :
+                  <div className="text-sm flex flex-col gap-2 py-2">
+                    <div className="flex gap-4">
+                      <div className="border-b">{option.account}</div>
+                      <div className="border-b">{option.contact.first_name + " " + option.contact.last_name}</div>
+                    </div>
+                    <div className="flex gap-2">
+                      <div className="border-b">{option.contact.address?.address1}</div>
+                      <div className="border-b">{option.contact.address?.address2}</div>
+                      <div className="border-b">{option.contact.address?.state}</div>
+                      <div className="border-b">{option.contact.address?.country}</div>
+                    </div>
+                  </div>
+              }
             </Box>
           );
         }}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            label="Choose a country"
-            inputProps={{
-              ...params.inputProps,
-              autoComplete: 'new-password', // disable autocomplete and autofill
-            }}
-          />
-        )}
+        renderInput={(params) => {
+          return (
+            <TextField {...params} variant="outlined" />
+          )
+        }}
       />
       <GenericForm {...{ ...props, fields: PartnerFormFields.BillingPartnerInformationFormFields }} />
     </div>
