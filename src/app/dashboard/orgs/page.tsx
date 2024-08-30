db2f980f51d39645a1182a88fae939085f537c7b
unknown <admin@shk-technology.com>
2024-08-30 02:20:20 -0500

    Update authenticate endpoint

--- src/app/dashboard/orgs/page.tsx
+++ src/app/dashboard/orgs/page.tsx
@@ -71,7 +71,7 @@ const Page = () => {
     handleOpenConfirmModal();
   }
   const handleSwitch = async () => {
-    const response = await fetch("https://license-management-server-lysrkspm1.vercel.app/authenticate/", {
+    const response = await fetch("https://license-management-server-lysrkspm1.vercel.app/authenticate", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
