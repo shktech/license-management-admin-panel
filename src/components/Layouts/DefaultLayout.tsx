d86a4bfee589bd94c0d432b5c0bb78dee925816c
le-roi-dev <chry.dev@outlook.com>
2024-09-12 22:52:19 +0300

    token small issue

--- src/components/Layouts/DefaultLayout.tsx
+++ src/components/Layouts/DefaultLayout.tsx
@@ -1,47 +1,47 @@
 "use client";
-import React, { useEffect, useState } from "react";
-import { usePathname } from "next/navigation";
+import React, { useState, ReactNode, useEffect } from "react";
 import Sidebar from "@/components/Sidebar";
 import { IconButton } from "@mui/material";
 import DoubleArrowIcon from "@mui/icons-material/DoubleArrow";
 import { Authenticated, useIsAuthenticated } from "@refinedev/core";
+import { usePathname } from "next/navigation";
+
 export default function DefaultLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
-  const pathname = usePathname();
-
   const handleOpenSidebar = () => {
     setSidebarOpen(true);
   };
 
+  const pathname = usePathname();
   const pageName = pathname.split("/")[2];
 
-  const { refetch } = useIsAuthenticated();
+  const { data, isSuccess, isLoading, isError, refetch } = useIsAuthenticated();
 
   useEffect(() => {
-    console.log(pageName);
     refetch();
   }, [pageName]);
-
   return (
-    <div className="flex h-screen overflow-hidden">
-      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-      <div
-        className={`flex flex-1 flex-col lg:ml-64 overflow-y-auto overflow-x-hidden`}
-      >
-        <main>
-          <div className="absolute top-0.5 left-0.5">
-            <IconButton sx={{ color: "#1f325c" }} onClick={handleOpenSidebar}>
-              <DoubleArrowIcon />
-            </IconButton>
-          </div>
+    <Authenticated key="any" appendCurrentPathToQuery={false}>
+      <div className="flex h-screen overflow-hidden">
+        <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+        <div
+          className={`flex flex-1 flex-col lg:ml-64 overflow-y-auto overflow-x-hidden`}
+        >
+          <main>
+            <div className="absolute top-0.5 left-0.5">
+              <IconButton sx={{ color: "#1f325c" }} onClick={handleOpenSidebar}>
+                <DoubleArrowIcon />
+              </IconButton>
+            </div>
 
-          <div className="mx-auto pt-4">{children}</div>
-        </main>
+            <div className="mx-auto pt-4">{children}</div>
+          </main>
+        </div>
       </div>
-    </div>
+    </Authenticated>
   );
 }
